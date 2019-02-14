bda5ae9efabbe50f8f350c8dc1a3c0a9559683b5
Morgre <36155906+Morgre@users.noreply.github.com>
2019-02-14 12:27:26 +0100

    First commit

--- main.m
+++ main.m
@@ -2,3 +2,24 @@
 % Description: Main File to solve the model on partial adjustment described
 % in Bongini (2018)
 global chi delta r xi tau_d tau_c A
+
+%% Parameter initalization
+chi = 0.25;
+delta = 1;
+r = 1;
+xi = 0.2;
+tau_d = 0.2;
+tau_c = 0.2;
+A = 1;
+
+%% create grid of shocks + Transition Matrix
+z = linspace(0.5, 1.5, 40);
+P = load("TransMat.mat",'-mat');
+TransMat = P.P;
+clear P;
+
+%% Solve constraint problem on the grid
+
+dev = 1;
+while abs(dev) > maxdev
+    Vnew = 
