dd1c9f979d88089af3d43088771409d8231fd686
Morgre <36155906+Morgre@users.noreply.github.com>
2019-03-24 17:04:23 +0100

    update

--- util.m
+++ util.m
@@ -1,35 +1,26 @@
 % Author: Moritz Greiwe
 % Description: Function that returns the utility for a given period for the
 % model according to the paper of Bongini (2018) on Partial Adjustment
-function util = util(z,k,kprime) % include p
-global chi delta r xi tau_d tau_c A
-a = length(k);
-b = length(z);
-k = kron(k,ones(a*b,1));
-kprime = kron(ones(b,1),kron(kprime, ones(a,1)));
-z = kron(ones(a*b,1),z);
+function util = util(k,p,kprime,pprime,z) % include p
+global chi delta r xi tau_d tau_c A s
 
 
 
 %calculate profits
-pi = A*z.*k.^chi;
-
-% check for indicator with y
-p =1/(1+r/(1+r))*(s*(1-delta)*k +(1-tau_c)*A*z(1)*k.^chi+tau_c*k);
-
+pi = A*z*k^chi;
 % get y
 y = pi - delta*k -r/(1+r)*p;
-
 % check for taxes on profits
-indicator = y>0
-g = tau_c*indicator.*y;
+g = tau_c*(y>0)*y;
 
 %check for taxes/flotation cost on cash flow
-c = pi-g-p-kprime+(1-delta).*k+pprime./(1+r);
-Psi1 = (c>0)*(1-tau_d);
-Psi2 = (c<0)*(1+xi);
-Psi = Psi1+Psi2
+c = pi-g-p-kprime+(1-delta)*k+pprime/(1+r);
+Psi = (c>0)*(1-tau_d)+(c<0)*(1+xi);
+
 
 %calculate utility
-util=Psi.*c
+util=Psi*c
+if (pprime > s*(1-delta)*kprime+pi-g)
+    util = -100000;
+end
 end
