a48364a0f240f173c2ea13b2b3369aaa2549f027
Morgre <36155906+Morgre@users.noreply.github.com>
2019-04-05 10:41:05 +0200

    Newest Update

--- main.m
+++ main.m
@@ -7,26 +7,26 @@ clear all
 clc
 
 %% Parameter initalization
-global chi delta r xi tau_d tau_c A s
+global chi delta r xi tau_d tau_c A s beta
 
 chi = 0.25;
 delta = 1;
-r = 1;
+r = 0.1;
 xi = 0.2;
 tau_d = 0.2;
 tau_c = 0.2;
 A = 1;
 s = 0.5;
-
+beta = 0.9
 %% create grid of shocks + Transition Matrix
 z = transpose(linspace(0.5, 1.5, 3));
 %P = load("TransMat.mat",'-mat'); for length(z)=40, check for smaller grid
 %first
 %TransMat = P.P;
-P = [0.5, 0.25, 0.25; 0.25, 0.5, 0.25; 0.25, 0.25, 0.5];
+P = [0.9, 0.05, 0.05; 0.05, 0.9, 0.05; 0.05, 0.05, 0.9];
 
 %% create grid for capital
-k_ss = 1;
+k_ss = (chi*A+z(1)/delta)^(1-chi);
 k_grid = linspace(0.5*k_ss, 1.5*k_ss, 7);
 knext_grid = linspace(0.5*k_ss, 1.5*k_ss, 7);
 
@@ -53,9 +53,9 @@ grid_matrix = [k_column, p_column, knext_column, pnext_column, z_column];
 %% Solve constraint problem on the grids using Value function iteration
 dev = 1;
 maxdev = 10^(-5); % Stopping condition
-maxiter = 10000;
+maxiter = 100000;
 iter = 0;
-V = k_ss*ones(length(k_grid)*length(p_grid)*length(z),1);
+V = 50*ones(length(k_grid)*length(p_grid)*length(z),1);
 utility = zeros(length(grid_matrix(:,1)),1);
 for i = 1:length(grid_matrix(:,1))
     utility(i,1)= util(grid_matrix(i,1), grid_matrix(i,2),...
@@ -65,7 +65,7 @@ while (max(abs(dev)) > maxdev) && (iter <= maxiter)
     V_help = reshape(V,length(z), length(V)/length(z));
     V_inter = P*V_help;
     V = reshape(V_inter, length(V),1);
-    V_util = kron(ones(length(k_grid)*length(p_grid),1),V)+utility;
+    V_util = beta*kron(ones(length(k_grid)*length(p_grid),1),V)+utility;
     V_util = reshape(V_util, length(z), length(k_grid)*length(p_grid), length(knext_grid)*length(pnext_grid));
     V_util = max(V_util,[], 2);
     Vnew = reshape(V_util, length(k_grid)*length(p_grid)*length(z),1);
